4a34104b4e62647b0469163991424e4b277c2f11
TannerDyck <97553206+TannerDyck@users.noreply.github.com>
2025-02-27 21:34:40 -0800

    Completely re-worked movement system and added materials.
    Added simple materials for all game objects, re-worked player movement from the ground-up and incorporated a dash and double jump. Still having issues with the player staying airborne when pushing against another collider.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -5,31 +5,24 @@ using UnityEngine.Events;
 public class InputManager : MonoBehaviour
 {
     public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
+    public UnityEvent OnJump = new UnityEvent();
+    public UnityEvent OnDash = new UnityEvent();
 
     void Update()
     {
-        Vector2 input = Vector2.zero;
-        if (Input.GetKey(KeyCode.W))
-        {
-            input += Vector2.up;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            input += Vector2.left;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            input += Vector2.down;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            input += Vector2.right;
-        }
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        Vector2 input = new Vector2(moveX, moveY).normalized;
+
+        OnMove?.Invoke(input);
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            OnJump?.Invoke();
         }
-        if (Input.GetKeyDown(KeyCode.LeftControl))
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
+            OnDash?.Invoke();
         }
         OnMove?.Invoke(input);
     }
